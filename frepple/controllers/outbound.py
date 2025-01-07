f6de39fede5e85dd9521b7e00e0287f851272c55
Johan De Taeye <jdetaeye@frepple.com>
2025-01-07 16:19:13 +0100

    fulton: bring quotes across as inquiries rather than quotes

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2070,8 +2070,8 @@ class exporter(object):
             ):
                 state = "done"
             if state in ("draft", "sent"):
-                # status = "inquiry"  # Inquiries don't reserve capacity and materials
-                status = "quote"  # Quotes do reserve capacity and materials
+                status = "inquiry"  # Inquiries don't reserve capacity and materials
+                # status = "quote"  # Quotes do reserve capacity and materials
                 qty = self.convert_qty_uom(
                     i["product_uom_qty"],
                     i["product_uom"],
