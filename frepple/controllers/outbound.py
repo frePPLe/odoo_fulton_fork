d8ebe46e21a72e30cb3e2339a0d631fb4296d22c
hichamlahlou <hlahlou@frepple.com>
2026-01-06 10:57:05 +0100

    append the id of the product at the end of the operation name

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1492,10 +1492,11 @@ class exporter(object):
                 for subcontractor in subcontractors:
                     # Build operation. The operation can either be a summary operation or a detailed
                     # routing.
-                    operation = "%s @ %s %d" % (
+                    operation = "%s @ %s %d %d" % (
                         product_buf["code"] or product_buf["name"],
                         subcontractor.get("name", location),
                         i["id"],
+                        product_id,
                     )
                     if len(operation) > 300:
                         suffix = " @ %s %d" % (
